256d4d2dc02d5e1a30eba51d56380ba7e42c0037
juanmanueldaza <juanmanueldaza@gmail.com>
2023-07-08 13:58:50 -0300

    first commit: scaffold y template

---
+++ src/main.js
@@ -0,0 +1,57 @@
+import p5 from "p5";
+import * as Tone from "tone";
+
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+let backgroundColor = 0;
+let lineColor = 255;
+
+const meter = new Tone.Meter();
+const mic = new Tone.UserMedia().connect(meter);
+mic
+  .open()
+  .then(() => {
+    // promise resolves when input is available
+    console.log("mic open");
+    // print the incoming mic levels in decibels
+    setInterval(() => console.log(meter.getValue()), 100);
+  })
+  .catch(e => {
+    // promise is rejected when the user doesn't have or allow mic access
+    console.log("mic not open");
+  });
+
+new p5(sketch => {
+  sketch.setup = () => {
+    sketch.createCanvas(windowWidth, windowHeight);
+    sketch.frameRate(30);
+    sketch.noFill();
+  };
+
+  sketch.draw = () => {
+    sketch.background(backgroundColor);
+
+    // Generate and draw moving lines
+    for (let i = 0; i < windowHeight; i += 10) {
+      let x1 = 0;
+      let y1 = i + sketch.random(-5, 5);
+      let x2 = windowWidth;
+      let y2 = i + sketch.random(-5, 5);
+
+      sketch.stroke(lineColor);
+      sketch.line(x1, y1, x2, y2);
+    }
+
+    // Add flickering effect
+    if (sketch.random() > 0.9) {
+      lineColor = sketch.random(255);
+    }
+  };
+
+  sketch.windowResized = () => {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    sketch.resizeCanvas(windowWidth, windowHeight);
+  };
+});
