7c1706a6a98887ee8409b3bf22bcbc0c81efb479
juanmanueldaza <juanmanueldaza@gmail.com>
2023-07-08 14:59:57 -0300

    updates in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/limites-visualization/",
+  build: {
+    assetsDir: "assets",
+    rollupOptions: {
+      input: "src/main.js",
+    },
+  },
 });
