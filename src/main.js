db993165321fab04c78c31d38dbff1c7103daead
juanmanueldaza <juanmanueldaza@gmail.com>
2023-07-08 16:00:03 -0300

    reactive to mic

--- src/main.js
+++ src/main.js
@@ -10,31 +10,43 @@ let lineColor = 255;
 const meter = new Tone.Meter();
 const mic = new Tone.UserMedia().connect(meter);
 
+let audioContextStarted = false; // Track if the audio context has been started
+
 new p5(sketch => {
   sketch.setup = () => {
     sketch.createCanvas(windowWidth, windowHeight);
     sketch.frameRate(30);
     sketch.noFill();
+
+    // Add event listener for user gesture
+    sketch.canvas.addEventListener("mousedown", startAudioContext);
+    sketch.canvas.addEventListener("touchstart", startAudioContext);
   };
 
   sketch.draw = () => {
     sketch.background(backgroundColor);
 
+    // Analyze the microphone input
+    const level = meter.getValue();
+
     // Generate and draw moving lines
     for (let i = 0; i < windowHeight; i += 10) {
       let x1 = 0;
-      let y1 = i + sketch.random(-5, 5);
+      let y1 = i + sketch.random(-5, 5) * level;
       let x2 = windowWidth;
-      let y2 = i + sketch.random(-5, 5);
+      let y2 = i + sketch.random(-5, 5) * level;
 
       sketch.stroke(lineColor);
       sketch.line(x1, y1, x2, y2);
     }
 
-    // Add flickering effect
+    // Add flickering effect based on the input level
     if (sketch.random() > 0.9) {
       lineColor = sketch.random(255);
     }
+
+    // Call draw() continuously for animation
+    sketch.loop();
   };
 
   sketch.windowResized = () => {
@@ -42,4 +54,20 @@ new p5(sketch => {
     windowHeight = window.innerHeight;
     sketch.resizeCanvas(windowWidth, windowHeight);
   };
+
+  // Function to start the audio context
+  function startAudioContext() {
+    if (!audioContextStarted) {
+      audioContextStarted = true;
+      mic
+        .open()
+        .then(() => {
+          Tone.start();
+          sketch.draw();
+        })
+        .catch(err => {
+          console.error("Failed to open microphone:", err);
+        });
+    }
+  }
 });
